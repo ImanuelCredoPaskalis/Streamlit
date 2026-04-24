131405800b2aa1cf829624c520df1b01aebe4cb9
Imanuel Credo <231414002@student.usd.ac.id>
2026-04-24 20:18:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ elif selected == "Analisis Grafik":
                 response = kirim_ke_model(
                     "Jelaskan kesalahan matematika dalam gambar tersebut dan berikan penjelasan yang mudah dipahami untuk memperbaikinya." \
                     "Jika tidak ada kesalahan, jelaskan apa yang benar dari grafik tersebut." \
-                    "Jika bukan gambar mengenai fungsi kuadrat, langsung kesimpulan bahwa gambar tersebut tidak relevan dengan fungsi kuadrat." \
+                    "Jika bukan gambar mengenai fungsi kuadrat, jangan berikan penjelasan, langsung kesimpulan bahwa gambar tersebut tidak relevan dengan fungsi kuadrat." \
                     ,image
                 )
                 st.session_state.hasil_analisis = {
